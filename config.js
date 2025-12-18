7757a70a12f76cf8dd6ad1044fd80648cd983ca6
DominicDev <s0l0estudi4r@gmail.com>
2025-12-18 13:30:49 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MARILYN MANSON';
+export let nameSongs = [
+	"KILLING STRANGERS", /*1*/
+	"KILL4ME", /*2*/
+	"THIS IS THE NEW SHIT", /*3*/
+	"THE BEAUTIFUL PEOPLE", /*4*/
+	"SWEET DREAMS", /*5*/
+	"THIRD DAY OF A SEVEN DAY BINGE", /*6*/
+	"DEEP SIX", /*7*/
+	"SAY10", /*8*/
+	"CRY LITTLE SISTER", /*9*/
+	"TAINTED LOVE", /*10*/
+	"PERSONAL JESUS", /*11*/
+	"MOBSCENE", /*12*/
+	"THE NOBODIES", /*13*/
+	"THE DOPE SHOW", /*14*/
+	"COMA WHITE", /*15*/
+	"THE FIGHT SONG", /*16*/
+	"TOURNIQUET", /*17*/
+	"LUNCHBOX", /*18*/
+	"DOPE HAT", /*19*/
+	"DISPOSABLE TEENS", /*20*/
+]
